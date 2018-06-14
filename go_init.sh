4fadb3e4a8e9beea9fda7523b5f7260e481fb92b
mofon <immofon@163.com>
2018-06-14 13:59:19 +0800

    add bash script for getting golang/x/...

---
+++ go_init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# utils
+function clone_x() {
+	git clone --depth 1 https://github.com/golang/$1.git
+	echo -e "\033[32minstalled: $1\033[0m"
+}
+
+function clone_x_all() {
+	for name in $@
+	do
+		echo -e "\033[32minstall: $name\033[0m"
+		clone_x $name
+	done
+}
+
+# begin
+cd $(go env GOPATH)
+
+mkdir -p src/golang/x/
+cd src/golang/x/
+
+packages="crypto net oauth2 vgo build tools sys mobile review net scratch debug exp image tour playground arch text perf lint time sync"
+
+clone_x_all $packages
