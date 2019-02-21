5f9b0a6ae2ad2e43c3ce45a45ebc425464dda8ab
mofon <yzkf000@foxmail.com>
2019-02-21 23:54:57 +0800

    add ln for UltiSnips

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 # use SSH instead of HTTPS to clone repos
 git config --global url."git@github.com:".insteadOf "https://github.com/"
 git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
+
+ln -s $(pwd)/UltiSnips ~/.vim/UltiSnips
