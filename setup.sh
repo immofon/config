b5647b37f40bc389efc54871704d1660775b304d
mofon <immofon@163.com>
2023-07-21 16:11:14 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,10 @@
 #git config --global url."git@github.com:".insteadOf "https://github.com/"
 #git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
 
+# nvim
 rm -rf ~/.config/nvim
-ln -S ./nvim ~/.config/nvim
+ln -s $(pwd)/nvim ~/.config/nvim
+
+# on-my-zsh
+rm -rf ~/.oh-my-zsh/custom/themes/xxf.zsh-theme
+ln -s $(pwd)/xxf.zsh-theme ~/.oh-my-zsh/custom/themes
