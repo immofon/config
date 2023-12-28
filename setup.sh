72bb3b71ba455782833131d8ecb756c07b52f03f
mofon <immofon@163.com>
2023-12-28 18:30:33 +0800

    update

--- setup.sh
+++ setup.sh
@@ -9,6 +9,14 @@
 rm -rf ~/.config/nvim
 ln -s $(pwd)/nvim ~/.config/nvim
 
+# tmux and tpm
+rm -rf ~/.tmux.conf
+ln -s  $(pwd)/.tmux.conf ~/.tmux.conf
+
+rm -rf ~/.tmux/plugins/tpm
+mkdir -p ~/.tmux/plugins
+git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+
 # on-my-zsh
 rm -rf ~/.oh-my-zsh/custom/themes/xxf.zsh-theme
 ln -s $(pwd)/xxf.zsh-theme ~/.oh-my-zsh/custom/themes
@@ -18,4 +26,6 @@ rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone git@github.com:zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone git@github.com:zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# tpm
