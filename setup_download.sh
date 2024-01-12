52e8e2d26b87a8b51166e94d5994a20b5902c3b3
Yang,Jingrong <immofon@163.com>
2024-01-12 16:04:35 +0800

    fix: using https to replace git for tpm

--- setup_download.sh
+++ setup_download.sh
@@ -3,7 +3,7 @@
 # tpm
 rm -rf ~/.tmux/plugins/tpm
 mkdir -p ~/.tmux/plugins
-git clone git@github.com:tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # omyzsh BEGIN
 rm -rf ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
